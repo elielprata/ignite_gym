9a5927e090898119e35784bafb0d3c08512ea08c
Eliel Martins <elieuprata@gmail.com>
2023-02-03 14:00:51 -0300

    create Route context

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import { Loading } from "@components/Loading";
 
 import { THEME } from "./src/theme";
 
-import { SignUp } from "@screens/SignUp";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -22,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
