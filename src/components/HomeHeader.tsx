0c99589a6148dcdacf125742f55f9239ffea9cba
Eliel Martins <elieuprata@gmail.com>
2023-02-03 19:28:23 -0300

    finalize Home Header Component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,4 +1,7 @@
-import { Heading, HStack, Text, VStack } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { Heading, HStack, Icon, Text, VStack } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+
 import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
@@ -10,7 +13,8 @@ export function HomeHeader() {
         size={16}
         mr={4}
       />
-      <VStack>
+
+      <VStack flex={1}>
         <Text color="gray.100" fontSize="md">
           Ola,
         </Text>
@@ -18,6 +22,10 @@ export function HomeHeader() {
           Eliel
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
     </HStack>
   );
 }
