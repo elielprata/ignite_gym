e645a976d013ca07bcaddb7ae8095bc774e6231c
Eliel Martins <elieuprata@gmail.com>
2023-02-04 08:46:53 -0300

    include Header, User Photo, and Edit photo button on Profile Screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,50 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { Center, ScrollView, Skeleton, Text, VStack } from "native-base";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { TouchableOpacity } from "react-native";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
   return (
-    <Center flex={1}>
-      <Text>Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              h={PHOTO_SIZE}
+              w={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: "https://github.com/elielprata.png" }}
+              alt="Imagem do Usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
