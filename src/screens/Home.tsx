6402011d6f8fbe6748c1c5f1980ba3522dc5e05d
Eliel Martins <elieuprata@gmail.com>
2023-02-03 22:11:34 -0300

    finalize style Group component and add state selected

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
+import { HStack, Text, VStack } from "native-base";
+
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { Text, VStack } from "native-base";
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState("costa");
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group name="costas" />
+      <HStack>
+        <Group
+          name="costa"
+          isActive={groupSelected === "costa"}
+          onPress={() => setGroupSelected("costa")}
+        />
+        <Group
+          name="ombro"
+          isActive={groupSelected === "ombro"}
+          onPress={() => setGroupSelected("ombro")}
+        />
+      </HStack>
     </VStack>
   );
 }
