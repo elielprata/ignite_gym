5615ece412e5689170aad5115c4d972d49e4eaee
Eliel Martins <elieuprata@gmail.com>
2023-02-04 07:21:39 -0300

    create Header of List

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FlatList, HStack, Text, VStack } from "native-base";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
 
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
@@ -33,6 +33,18 @@ export function Home() {
         my={10}
         maxH={10}
       />
+
+      <VStack flex={1} px={8} mb={5}>
+        <HStack justifyContent="space-between">
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
