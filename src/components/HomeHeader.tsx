1a92657094b4cd4c8edde54d8924a7301321982d
Eliel Martins <elieuprata@gmail.com>
2023-02-06 12:22:03 -0300

    show user name using context

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -3,8 +3,11 @@ import { Heading, HStack, Icon, Text, VStack } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import { UserPhoto } from "./UserPhoto";
+import { useAuth } from "@hooks/useAuth";
 
 export function HomeHeader() {
+  const { user } = useAuth();
+
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto
@@ -20,7 +23,7 @@ export function HomeHeader() {
         </Text>
 
         <Heading color="gray.100" fontSize="md" fontFamily="heading">
-          Eliel
+          {user.name}
         </Heading>
       </VStack>
 
