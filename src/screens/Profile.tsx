e29da5c36ebc446b4da8e3ac3616871555f099c9
Eliel Martins <elieuprata@gmail.com>
2023-02-07 10:07:41 -0300

    update photo on context

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -115,11 +115,19 @@ export function Profile() {
         const userPhotoUploadForm = new FormData();
         userPhotoUploadForm.append("avatar", photoFile);
 
-        await api.patch("/users/avatar", userPhotoUploadForm, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        });
+        const avatarUpdatedResponse = await api.patch(
+          "/users/avatar",
+          userPhotoUploadForm,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+
+        const userUpdated = user;
+        userUpdated.avatar = avatarUpdatedResponse.data.avatar;
+        updateUserProfile(userUpdated);
 
         toast.show({
           title: "Foto atualizada",
